2657673cebc6e677a65f78d993b976227f52cd4b
rehagursel <92670670+rehagursel@users.noreply.github.com>
2022-03-19 16:29:34 +0300

    login component validation feature updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedLoggedInInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedLoggedInInfo === "true") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // authentication will be added
+    localStorage.setItem("isLoggedIn", "true");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
